b045287ac7939daefbf87d9fcbba58142bc41f7b
ukanyarat <150550152+ukanyarat@users.noreply.github.com>
2024-03-29 18:01:24 +0700

    Add files via upload

---
+++ Dice/Dice/Dice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    internal class Dice
+    {
+        private int die;
+        private int face;
+        private Random rnd;
+
+        //rnd = new Random(); //เรียกคอนสตักเกตอร์ไปรับค่า
+        //rnd.Next(1,7)//for random ในเน็กแรนดอมเริ่ม ถึงที่สิ้นสุด+1 = คือจะได้1-6  use class random
+
+        public int Face
+        {
+            get { return face; }
+            set { face = value; }
+        }
+        public Dice()
+        {
+            face = 6;
+            rnd = new Random();
+            die = rnd.Next(1, 7);
+        }
+        public Dice(int f)
+        {
+            face = f;
+            rnd = new Random();
+            die = rnd.Next(1, face + 1);
+        }
+        public int RollDice()
+        {
+            die = rnd.Next(1, face + 1);
+            return die;
+        }
+    }
+}
---
+++ Dice/Dice/Programs.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    internal class Programs
+    { 
+
+        public static void Main(string[] args)
+    {   //ลองทำ
+        //Dice dice1= new Dice();
+
+        //Console.WriteLine("Dice#1 has {0} face",dice1.Face);
+        //Console.WriteLine("Dice#2 has {0} face",dice1.Face);
+        //int result1, result2, count = 0;
+
+        //do {
+        //    result1 = dice1.RollDice();
+        //    result2= dice1.RollDice();
+        //    Console.WriteLine("Round {0}: ",count+1);
+        //    Console.WriteLine("Dice#1 = {0}",result1);
+        //    Console.WriteLine("Dice#2 = {0}",result2);
+        //    Console.WriteLine("=============");
+        //    count++;
+        //}while(result1 != result2);
+        //Console.WriteLine("Roll 2 dices {0} times to get the same value!!!",count);
+
+
+        //or or or or or or or or or or or or or or or or or or or or or or or or or or or -----------------------
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice(6);
+        int die1;
+        int die2;
+        int count = 0; //ไว้นับ เเล้วเริ่มต้นที่0
+        Console.WriteLine("Dice 1 has {0} face", dice1.Face);
+        Console.WriteLine("Dice 2 has {0} face", dice2.Face);
+
+
+        die1 = dice1.RollDice();
+        die2 = dice2.RollDice();
+        //Console.WriteLine("Dice#1 = {0}", die1);
+        //Console.WriteLine("Dice#1 = {0}", die2);
+        //Console.WriteLine("=============");
+
+        //----------------------- while
+        //while (die1 != die2) {
+        //    count++;
+        //    Console.WriteLine("Round {0}: ", count);
+        //    die1 = dice1.RollDice();
+        //    die2 = dice2.RollDice();
+        //    Console.WriteLine("Dice#1 = {0}", die1);
+        //    Console.WriteLine("Dice#1 = {0}", die2);
+        //    Console.WriteLine("=============");
+        //}
+        //----------------------- do while
+        do
+        {
+            count++;
+            Console.WriteLine("Round {0}: ", count);
+            die1 = dice1.RollDice();
+            die2 = dice2.RollDice();
+            Console.WriteLine("Dice#1 = {0}", die1);
+            Console.WriteLine("Dice#1 = {0}", die2);
+            Console.WriteLine("=============");
+        } while (die1 != die2);
+
+        Console.WriteLine("Roll 2 dices {0} times to get the same value!!!", count);
+
+    }
+}
+}
